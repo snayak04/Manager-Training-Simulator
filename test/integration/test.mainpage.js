672e58ca8ceb762acf8a0ba29897f5da3fafcb5d
Swarup Nayak <snayak04.96@gmail.com>
2018-09-13 16:22:34 -0400

    edited some teting files

--- test/integration/test.mainpage.js
+++ test/integration/test.mainpage.js
@@ -24,26 +24,26 @@ casper.test.begin('Watson Assistant simple Demo', 5, function suite(test) {
     });
   }
 
-  // function testEnterMessageClick() {
-  //   casper.then(function () {
-  //     this.sendKeys('#textInput', 'turn the wipers on');
-  //     this.sendKeys('#textInput', casper.page.event.key.Enter);
-  //   });
-  //   casper.waitForSelector('.from-user', function () {
-  //     test.assertExists('.message-inner', 'Message sent');
-  //     test.assertTextExists('turn the wipers on', 'Message in bubble');
-  //     casper.waitForText('I\'ll turn on the wipers for you.');
-  //   });
-  // }
+  function testEnterMessageClick() {
+    casper.then(function () {
+      this.sendKeys('#textInput', 'turn the wipers on');
+      this.sendKeys('#textInput', casper.page.event.key.Enter);
+    });
+    casper.waitForSelector('.from-user', function () {
+      test.assertExists('.message-inner', 'Message sent');
+      test.assertTextExists('turn the wipers on', 'Message in bubble');
+      casper.waitForText('I\'ll turn on the wipers for you.');
+    });
+  }
 
-  // casper.start(baseHost, function () {
-  //   casper.test.comment('Starting Testing');
-  //   test.assertHttpStatus(200, 'assistant-simple is up');
-  //   test.assertTitle('Watson Assistant Chat App', 'Title is correct');
+  casper.start(baseHost, function () {
+    casper.test.comment('Starting Testing');
+    test.assertHttpStatus(200, 'assistant-simple is up');
+    test.assertTitle('Watson Assistant Chat App', 'Title is correct');
 
-  //   testWelcomeMessageExists();
-  //   testEnterMessageClick();
-  // });
+    testWelcomeMessageExists();
+    testEnterMessageClick();
+  });
 
   casper.run(function () {
     test.done();
