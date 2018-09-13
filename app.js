47a97a67947da9306345d91877c72dd59f7f8b26
Paul <gillen.58@osu.edu>
2018-09-13 16:55:26 -0400

    Created framework for handling different intents on the backend.

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ var express = require('express'); // app server
 var bodyParser = require('body-parser'); // parser for post requests
 var AssistantV1 = require('watson-developer-cloud/assistant/v1'); // watson sdk
 var TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
+var intentHandlers = require('./public/js/intents')
 
 var app = express();
 
@@ -118,23 +119,27 @@ function updateMessage(input, response) {
   var responseText = null;
   if (!response.output) {
     response.output = {};
-  } else {
-    return response;
-  }
+  } 
   if (response.intents && response.intents[0]) {
     var intent = response.intents[0];
-    // Depending on the confidence of the response the app can return different messages.
-    // The confidence will vary depending on how well the system is trained. The service will always try to assign
-    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
-    // user's intent . In these cases it is usually best to return a disambiguation message
-    // ('I did not understand your intent, please rephrase your question', etc..)
-    if (intent.confidence >= 0.75) {
-      responseText = 'I understood your intent was ' + intent.intent;
-    } else if (intent.confidence >= 0.5) {
-      responseText = 'I think your intent was ' + intent.intent;
-    } else {
-      responseText = 'I did not understand your intent';
-    }
+	
+	switch(intent.intent){
+		case "Wait":
+			responseText = intentHandlers.wait();
+			break;
+		case "Tasks":
+			responseText = intentHandlers.tasks();
+			break;
+		case "Project":
+			responseText = intentHandlers.project();
+			break;
+		case "MyEmployees":
+			responseText = intentHandlers.myEmployees();
+			break;
+		
+		
+	}
+
   }
   response.output.text = responseText;
   return response;
