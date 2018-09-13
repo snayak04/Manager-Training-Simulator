2244a08ca237a6dfe8e877ecfd23f002fc83d36e
Paul <gillen.58@osu.edu>
2018-09-13 18:23:58 -0400

    Added more intents, made empty files for classes, updated workspace

---
+++ public/js/entities/employee.js
@@ -0,0 +1,11 @@
+//Defines a class for managing employee objects
+
+function Employee(){
+	//who knows
+}
+
+
+
+module.exports = {
+	Employee: Employee
+}
---
+++ public/js/entities/task.js
@@ -0,0 +1,11 @@
+//Defines a class for managing task objects
+
+function Task(){
+	//who knows
+}
+
+
+
+module.exports = {
+	Task: Task
+}
