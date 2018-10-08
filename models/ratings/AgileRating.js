e0bf8d37d57bf257984d5bcccd05931bf31fce80
Swarup Nayak <snayak04.96@gmail.com>
2018-10-08 15:04:48 -0500

    Started Agile Rating

--- models/ratings/AgileRating.js
+++ models/ratings/AgileRating.js
@@ -1,11 +1,21 @@
 
-const Rating = require('./Rating');
-
-class AgileRating{
-    constructor(){
-    }
-
-    analyzeAndScore(context){
-        console.log(context);
-    }
+function AgileRating() {
+    this.score = 0;
 }
+
+/**
+ * @returns this.score
+ */
+AgileRating.prototype.getScore = () => {
+    return this.score;
+};
+
+/**
+ * Always listens whenever a response is sent by Assistant API.
+ * @param {JSON object that is returned by the IBM Assistant} context 
+ */
+AgileRating.prototype.listen = (context) => {
+    console.log(context);
+};
+
+module.exports = AgileRating;
