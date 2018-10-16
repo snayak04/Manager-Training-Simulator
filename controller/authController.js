4a6fe8af336e97c86304d5fec945090eaa3a4e78
Paul <gillen.58@osu.edu>
2018-10-16 18:35:58 -0400

    Login page functionality and session work, need to update database checks

--- controller/authController.js
+++ controller/authController.js
@@ -1,14 +1,19 @@
 var mongoose = require("mongoose");
 var passport = require("passport");
 var User = require("../models/user");
-var root = 'public';
+var root = 'views';
 
 var userController = {};
 
 // Restrict access to root page
 userController.home = function(req, res) {
   console.log('Restrict root page for logged in users')
-  res.sendFile('index', { user : req.user, 'root': root});
+  if(req.user){
+	res.sendFile('index.html', { user : req.user, 'root': root});
+  }
+  else{
+	res.sendFile('login.html', {'root': root});
+  }
 };
 
 // Go to registration page
@@ -18,7 +23,6 @@ userController.register = function(req, res) {
 
 // Post registration
 userController.doRegister = function(req, res) {
-  console.log(req.body)
   User.register(new User({ username : req.body.username}), req.body.password, function(err, user) {
     if (err) {
       return res.sendFile('register.html', { user : user , 'root': root});
