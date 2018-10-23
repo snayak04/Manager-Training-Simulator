69731c57f676b7cfe21800c191b0992e44cf18f8
Paul Gillen <gillen.58@osu.edu>
2018-10-23 14:00:52 -0400

    Updated login test, deleted mainpage as it is included in login

--- test/integration/test.login.js
+++ test/integration/test.login.js
@@ -9,37 +9,46 @@ casper.test.begin('Login Tests', 2, function suite(test) {
     test.assertTitle('Not A Game - Manager Training Simulator Login Page', 'Title is correct');
     test.assertExists("form[action='/login']", 'login form is found');
     test.assertExists("form[action='/register']", 'register button is found');
-    
-    //test register page
     this.fill("form[action='/register']", {}, true); //go to register page
-    test.assertTitle('Not A Game - Manager Training Simulator Registration Page', 'Title is correct');
-    test.assertUrlMatch(/register/, 'on register page');
-    test.assertExists("form[action='/login']", 'login button is found');
-    test.assertExists("form[action='/register']", 'register form is found');
-    
-    this.fill("form[action='/register']", {}, true); //go to register page
-    test.assertTitle('Not A Game - Manager Training Simulator Registration Page', 'Title is correct');
-    test.assertUrlMatch(/register/, 'on register page');
-    test.assertExists("form[action='/login']", 'login button is found');
-    test.assertExists("form[action='/register']", 'register form is found');
-    
-    this.fill("form[action='/register']", { //create new user, should redirect to index
-        'username': Travis,
-        'password': Password
-    }, true);
-    test.assertTitle('Not A Game - Manager Training Simulator', 'Title is correct');
-    test.assertExists("form[action='/logout']", 'logout button found');
-    this.fill("form[action='/logout']", {}, true); //return to login page
-    
-    test.assertTitle('Not A Game - Manager Training Simulator Login Page', 'Title is correct');
-    test.assertUrlMatch(/login/, 'on register page');
-    this.fill("form[action='/login']", { //test that user can login again
-        'username': Travis,
-        'password': Password
-    }, true);
-    test.assertTitle('Not A Game - Manager Training Simulator', 'Title is correct');
-
   });
+    
+
+    //test register page
+    casper.then(function() {
+      casper.test.comment('Testing Registration');
+      test.assertTitle('Not A Game - Manager Training Simulator Registration Page', 'Title is correct');
+      test.assertUrlMatch(/register/, 'on register page');
+      test.assertExists("form[action='/login']", 'login button is found');
+      test.assertExists("form[action='/register']", 'register form is found');
+      this.fill("form[action='/register']", { //create new user, should redirect to index
+        'username': Travis,
+        'password': Password
+      }, true);
+    });
+    
+    //test logout
+    casper.then(function(){
+      casper.test.comment('Testing Logout');
+      test.assertTitle('Not A Game - Manager Training Simulator', 'Title is correct');
+      test.assertExists("form[action='/logout']", 'logout button found');
+      this.fill("form[action='/logout']", {}, true); //return to login page
+    });
+    
+    //test login
+    casper.then(function()){
+      casper.test.comment('Testing Login');
+      test.assertTitle('Not A Game - Manager Training Simulator Login Page', 'Title is correct');
+      test.assertUrlMatch(/login/, 'on register page');
+      this.fill("form[action='/login']", { //test that user can login again
+          'username': Travis,
+          'password': Password
+      }, true);
+    });
+    
+    //check login worked
+    casper.then(function()){
+      test.assertTitle('Not A Game - Manager Training Simulator', 'Title is correct');
+    });
 
   casper.run(function () {
     test.done();
